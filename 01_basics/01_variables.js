4fe42ae3b65f01c3b106ea1abb71c53dce90ad30
gaurav pandey <128570258+Gauravpandey001@users.noreply.github.com>
2023-11-24 18:06:09 +0000

    not to use var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1434
+let accountEmail = "gauravpandeyji44@gmail.com"
+var accountPassword = "12345"
+accountCity = "dehradun"
+
+// accountId = 2 (not  allowed)
+accountEmail = "asdas@hasdj.com"
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountEmail , accountId , accountPassword , accountCity]);
