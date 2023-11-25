e6057c6732d2d495dc7305137013731149faddb7
gaurav pandey <128570258+Gauravpandey001@users.noreply.github.com>
2023-11-25 19:11:57 +0000

    data_types

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -5,3 +5,20 @@
 console.log(3+3); 
 
 console.log(3+3)
+let age = 18
+let isLoggedin = false
+let state = null
+let name = "gaurav"
+
+//number
+//bigint
+//boolean
+//null (Stanndalone value)
+//undefined
+//symbol (unique)
+
+
+
+//object
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
