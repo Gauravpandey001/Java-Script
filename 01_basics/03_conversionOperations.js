763d253684e3be3ad8a9875ee4785b39d88dc8d0
gaurav pandey <128570258+Gauravpandey001@users.noreply.github.com>
2023-11-26 12:02:48 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -20,3 +20,11 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+//*******************operations*****************
+let value = 3
+let negvalue = -value
+console.log(negvalue) 
+let str1 = "raaju"
+let str2 = " maacho"
+let str3 = (str1+str2)
+console.log(str3)
